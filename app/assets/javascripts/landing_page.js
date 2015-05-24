ed8c968490ca7f5fa68bf77de8483e0d1964af37
yaysunshine <merida1592@gmail.com>
2015-05-23 19:01:35 -0700

    shaker div shakes on hover

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-  alert("hereeee");
+  console.log("hereeee");
+
+  // $("#shaker").toggle()
+
+  // $("#shaker").hover(function(){
+  //     $("#shaker").fadeIn();
+  // },function(){
+  //     $("#shaker").fadeOut();
+  // });
+
+  jQuery.fn.shake = function() {
+      this.each(function(i) {
+          $(this).css({ "position" : "relative" });
+          for (var x = 1; x <= 3; x++) {
+              $(this).animate({ left: -25 }, 10).animate({ left: 0 }, 50).animate({ left: 25 }, 10).animate({ left: 0 }, 50);
+          }
+      });
+      return this;
+  }
+
+  $("#shaker").hover(function() {
+      $(this).shake();
+  });
 });
