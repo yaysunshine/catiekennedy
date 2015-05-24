a0790f544f3af08006b254b5c42f69681da6eca2
yaysunshine <merida1592@gmail.com>
2015-05-23 19:42:54 -0700

    localhost is reading landing_page.js on master branch now - remove coffeescript, rm -rf public/assets, and remove turbolinks from application.html.erb and application.js and gemfile and re-bundle

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log("hereeee");
+  alert("hereeee");
+
+  jQuery.fn.shake = function() {
+      this.each(function(i) {
+          $(this).css({ "position" : "relative" });
+          for (var x = 1; x <= 3; x++) {
+              $(this).animate({ left: -25 }, 10).animate({ left: 0 }, 50).animate({ left: 25 }, 10).animate({ left: 0 }, 50);
+          }
+      });
+      return this;
+  }
+
+  $("#shaker").hover(function() {
+      $(this).shake();
+  });
 });
